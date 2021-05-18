b9c288f479c172caa36f7505caecb1aaca6a6805
rahul975337 <tilwani03@gmail.com>
2021-05-18 17:09:22 +0530

    done til 2nd vid

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  ApiConstants._();
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = '08cdb8963193e939c597034e9e54a3a7';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+}
